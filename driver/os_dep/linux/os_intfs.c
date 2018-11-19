b09b976c4a251ae084e317372892f8a2d4dd587e
coolshou <coolshou2060@gmail.com>
2018-11-19 13:17:33 +0800

    fix kernel 4.19

--- driver/os_dep/linux/os_intfs.c
+++ driver/os_dep/linux/os_intfs.c
@@ -1263,7 +1263,11 @@ unsigned int rtw_classify8021d(struct sk_buff *skb)
 	return dscp >> 5;
 }
 
-
+#if (LINUX_VERSION_CODE>=KERNEL_VERSION(4,19,0)) //jimmy, 20181112
+static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb,
+				struct net_device *sb_dev,
+				select_queue_fallback_t fallback)
+#else
 static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 13, 0)
 	, void *accel_priv
@@ -1272,6 +1276,7 @@ static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb
 	#endif
 #endif
 )
+#endif
 {
 	_adapter	*padapter = rtw_netdev_priv(dev);
 	struct mlme_priv *pmlmepriv = &padapter->mlmepriv;
